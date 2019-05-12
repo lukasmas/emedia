537a83c24a08dd8c22d2a97f0916392e2d689844
lukasmas <mastalerzl12@gmail.com>
2019-05-12 12:50:47 +0200

    first commit

---
+++ emedia/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace emedia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
